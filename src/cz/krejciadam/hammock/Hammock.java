b1b11ccc33649f557b0f981403af7382b7eebc3a
Adam Krejci <krejciadam@gmail.com>
2018-03-15 15:10:00 +0100

    Changelog, manual, in-line manual

--- src/cz/krejciadam/hammock/Hammock.java
+++ src/cz/krejciadam/hammock/Hammock.java
@@ -36,7 +36,7 @@ public class Hammock {
     private static final String PARENT_DIR = (new File(Hammock.class.getProtectionDomain().getCodeSource().getLocation().getPath()).getParentFile().getParentFile().getPath());
 
     //common
-    private static final String VERSION = "1.1.4";
+    private static final String VERSION = "1.2.0";
     private static List<UniqueSequence> initialSequences = null;
     private static String inputFileName = null;
     public static String workingDirectory = null;
@@ -322,6 +322,7 @@ public class Hammock {
         //System.err.println("-a, --part_threshold <float [0, 1]>\n\tThe proportion of clusters to be used as cores\n");
         //System.err.println("-s, --size_threshold <int\n\tMinimal size (in unique sequences) of a cluster to be used a core\n");
         System.err.println("-c, --count_threshold <int>\n\tThis many largest (in terms of total or unique size) initial clusters will be used as cluster cores. \n");
+        System.err.println("-E, --initial_extension_threshold <float>\n\tA score threshold used in the initial cluster extension step \n");
         System.err.println("-n, --assign_thresholds <float,float,float...>\n\tA sequence of threshold values for sequence to cluster assignment\n");
         System.err.println("-v, --overlap thresholds <float,float,float...\n\tA sequence of threshold values of min. cluster overlap\n");
         System.err.println("-r, --merge_thresholds <float,float,float...>\n\tA sequence of threshold values for cluster-cluster comparisons\n");
